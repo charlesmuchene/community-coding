2aab8920f6302e63091865e09fed4044492911c1
Charles Muchene <charlesmuchene@gmail.com>
2018-06-13 15:28:20 +0300

    Adds introduction lesson

--- src/main/kotlin/com/charlesmuchene/cc/Application.kt
+++ src/main/kotlin/com/charlesmuchene/cc/Application.kt
@@ -2,6 +2,107 @@ package com.charlesmuchene.cc
 
 fun main(args: Array<String>) {
 
-    // Ready to rock?
+    // Oneline comment
+
+    /**
+     * Multiline comment
+     *
+     * This spans several lines
+     */
+
+
+
+    // Data Types
+    // ===========================
+
+    // var age: Int = 2
+
+    // var memory: Byte = 2
+
+    // var initial: Char = 'C'
+
+    // var temperature: Float = 3.4
+
+    // var company: String  = "SafeBoda"
+
+
+
+
+    // Immutability - var vs val
+    // ===========================
+
+    // var a: Int = 0
+
+    // a = 3
+
+    // val b: Int = 0
+
+    // b = 3
+
+
+
+
+    // Nullability of types
+    // ===========================
+
+    // var age: Int = null
+
+
+
+
+
+    // Type inference
+    // ===========================
+
+    // val million: Long = 1_000_000
+
+    // val thousand = 1_000
+
+    // val country = "Uganda"
+
+
+
+
+    // Arithmetic operators
+    // ===========================
+
+    // val numberOne = 3
+    // val numberTwo = 67
+
+    // val sum = numberOne + numberTwo
+
+    // val division = numberTwo / numberOne
+
+    // val remainder = numberTwo % numberOne
+
+
+
+
+    // Logical operators
+    // ===========================
+
+    // val x = false
+    // val y = true
+
+    // val and = x && y
+
+    // val or = x || y
+
+
+
+
+    // Comparison operators
+    // ===========================
+
+    // val orange = "Orange"
+    // val apple = "Apple"
+
+    // val same = orange != apple
+
+    // val greater = orange > apple
+
+    // val lessOrEqual = apple <= orange
+
 
 }
+
